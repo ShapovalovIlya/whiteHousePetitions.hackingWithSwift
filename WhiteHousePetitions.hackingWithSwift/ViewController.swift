692974fe16d19387c305294344eed9c9ca9d0ec1
Ilia Shapovalov <greateman@mail.ru>
2022-06-27 21:50:43 +0300

    initial commit

---
+++ WhiteHousePetitions.hackingWithSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhiteHousePetitions.hackingWithSwift
+//
+//  Created by Илья Шаповалов on 27.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
